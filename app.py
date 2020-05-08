bc8865d104cf50e64706f8848fafeb15c85bdd77
mariakesa <maria.kesa@gmail.com>
2020-05-09 00:48:43 +0200

    updates

--- app.py
+++ app.py
@@ -10,22 +10,19 @@ To run type: "streamlit run app.py" in the terminal.
 def load_data(path,countries_path):
   dat=pd.read_csv(path)
   dat.rename(columns={'country':'country_code'},inplace=True)
-  dat=dat['country_code'].dropna()
-  st.write(dat)
+  dat_cntr=dat['country_code'].dropna()
   countries=pd.read_csv(countries_path)
-  print(countries.columns)
   countries.rename(columns={'country':'country_code'},inplace=True)
-  st.write(countries)
-  countries_geo=pd.merge(dat,countries,on='country_code')
-  st.write(countries_geo)
-  return countries_geo
+  countries_geo=pd.merge(dat_cntr,countries,on='country_code')
+  return dat,countries_geo
 
 
 path='25100_dat.csv'
 #Countries downloaded from 'https://developers.google.com/public-data/docs/canonical/countries_csv')
 countries_path='countries.csv'
-data=load_data(path,countries_path)
+full_data,countries_data=load_data(path,countries_path)
+st.subheader('Raw data')
+if st.checkbox("Show Raw Data", False):
+    st.write(full_data)
 
-st.map(data)
-
-st.write(data)
+st.map(countries_data)
