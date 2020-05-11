b2c6595d0becddeb9c9914ab1a3edb63dd57dfd8
mariakesa <maria.kesa@gmail.com>
2020-05-11 02:36:05 +0200

    Add death_geo func

--- app.py
+++ app.py
@@ -11,6 +11,21 @@ from streamlit import caching
 
 caching.clear_cache()
 
+def prepare_death_geo(df):
+    death_df=df.copy()
+    alpha=pd.read_csv(alpha_path,header=0)
+    #alpha.rename(columns={'Alpha-2 code':'country_code'},inplace=True)
+    #print(death_df)
+    alpha['country_code'] = alpha['country_code'].map(lambda x: x.replace('\"','').strip())
+    alpha['Alpha-3 code'] = alpha['Alpha-3 code'].map(lambda x: x.replace('\"','').strip())
+    cntr_dict={}
+    for ind in range(0,alpha.shape[0]):
+      cntr_dict[alpha.iloc[ind]['country_code']]=alpha.iloc[ind]['Alpha-3 code']
+    death_geo = death_df[death_df['country_code'].notna()]
+    death_geo['country_code']=death_geo['country_code'].map(cntr_dict)
+    death_geo=death_geo.groupby(['country_code']).sum()
+    death_geo['country_code']=death_geo.index
+    return death_geo
 
 
 @st.cache(persist=True)
@@ -22,20 +37,7 @@ def load_data(path,countries_path,alpha_path):
   countries.rename(columns={'country':'country_code'},inplace=True)
   countries_geo=pd.merge(dat_cntr,countries,on='country_code')
   #Death map
-  death_df=dat.copy()
-  #['death','country_code'].dropna(how='any')
-  alpha=pd.read_csv(alpha_path,header=0)
-  #alpha.rename(columns={'Alpha-2 code':'country_code'},inplace=True)
-  #print(death_df)
-  alpha['country_code'] = alpha['country_code'].map(lambda x: x.replace('\"','').strip())
-  alpha['Alpha-3 code'] = alpha['Alpha-3 code'].map(lambda x: x.replace('\"','').strip())
-  cntr_dict={}
-  for ind in range(0,alpha.shape[0]):
-    cntr_dict[alpha.iloc[ind]['country_code']]=alpha.iloc[ind]['Alpha-3 code']
-  death_geo = death_df[death_df['country_code'].notna()]
-  death_geo['country_code']=death_geo['country_code'].map(cntr_dict)
-  death_geo=death_geo.groupby(['country_code']).sum()
-  death_geo['country_code']=death_geo.index
+  death_geo=prepare_death_geo(dat)
   #death_geo.rename(columns={'country_code':'deaths'},inplace=True)
   return dat,countries_geo, death_geo
 
